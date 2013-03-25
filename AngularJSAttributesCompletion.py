b80e8403c6501168e73e7d272a99abdfe90aa664
subhaze <jmrussell22@gmail.com>
2013-03-24 22:28:50 -0400

    adds support for custom components

--- AngularJSAttributesCompletion.py
+++ AngularJSAttributesCompletion.py
@@ -18,14 +18,18 @@ class AngularJSAttributesCompletion(sublime_plugin.EventListener):
 		# check if we are inside a tag
 		is_inside_tag = view.match_selector(locations[0],
 				"text.html meta.tag - text.html punctuation.definition.tag.begin")
-		# return self.completions()
-		if is_inside_tag:
-			return self.completions()
-		else:
-			return []
 
-	def completions(self):
-		core_attrs = [tuple(attr) for attr in self.settings.get('core_attribute_list')]
-		extended_attrs = [tuple(attr) for attr in self.settings.get('extended_attribute_list')]
-		extended_attrs += core_attrs
-		return (extended_attrs, sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
+		return self.completions(is_inside_tag)
+
+	def completions(self, is_inside_tag):
+		if is_inside_tag :
+			core_attrs = [tuple(attr) for attr in self.settings.get('core_attribute_list')]
+			extended_attrs = [tuple(attr) for attr in self.settings.get('extended_attribute_list')]
+			extended_attrs += core_attrs
+			return (extended_attrs, sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
+
+		if not is_inside_tag:
+			components = []
+			for component in self.settings.get('angular_components'):
+				components.append((component + "\tAngularJS Component", component + "$1>$2</" + component + '>'))
+			return components
