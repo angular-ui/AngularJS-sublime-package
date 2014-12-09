7f145590c69e37dbc4a5e1ed0bdbc01d665c17d3
Thibault Leruitte <thibault@leruitte.name>
2014-12-09 11:38:10 +0100

    Fix completions when data- prefix is enabled
    Fix completions inside ng-controller and ng-app

--- AngularJS-sublime-package.py
+++ AngularJS-sublime-package.py
@@ -388,9 +388,9 @@ class AngularJSEventListener(sublime_plugin.EventListener):
 				return jscompletions.get(('module'), ng.get_current_project_indexes())
 			return jscompletions.in_string_completions(prefix, ng.get_current_project_indexes())
 
-		if(viewlocation.at_html_attribute(view, 'ng-controller', locations)):
+		if(viewlocation.at_html_attribute(view, ('data-' if ng.settings.get('enable_data_prefix') else '') + 'ng-controller', locations)):
 			return jscompletions.get(('controller'), ng.get_current_project_indexes())
-		if(viewlocation.at_html_attribute(view, 'ng-app', locations)):
+		if(viewlocation.at_html_attribute(view, ('data-' if ng.settings.get('enable_data_prefix') else '') + 'ng-app', locations)):
 			return jscompletions.get(('module'), ng.get_current_project_indexes())
 		if(view.score_selector(_scope, ng.settings.get('filter_scope'))):
 			return ng.filter_completions()
